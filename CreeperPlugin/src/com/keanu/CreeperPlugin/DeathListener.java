48e8a6a4b9a03310e25e6bc26a19953aff736220
Keanu Nichols <nichols.keanu9@gmail.com>
2021-03-21 13:57:25 -0400

    Add files via upload

--- CreeperPlugin/src/com/keanu/CreeperPlugin/DeathListener.java
+++ CreeperPlugin/src/com/keanu/CreeperPlugin/DeathListener.java
@@ -3,11 +3,13 @@ package com.keanu.CreeperPlugin;
 //import org.bukkit.plugin.java.JavaPlugin;
 import java.util.Random;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 //import org.bukkit.entity.Cow;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
 
 public class DeathListener implements Listener{
 	
@@ -22,10 +24,14 @@ public class DeathListener implements Listener{
 
 	@EventHandler
 	public void onEntityDeath(EntityDeathEvent event){
+		int random_number;
 		if(getRandomBoolean()){
 			if(event.getEntity() instanceof LivingEntity){
 					//event.getEntity().getWorld().createExplosion(event.getEntity().getLocation(), 1, true, true, event.getEntity());
-				event.getEntity().getWorld().createExplosion(event.getEntity().getLocation(), getRandomNumber(1,5), true, true, event.getEntity());
+				random_number = getRandomNumber(1,10);
+				Player plr = Bukkit.getPlayer("KeanuNichols");
+				plr.sendMessage("Power level" + random_number);
+				event.getEntity().getWorld().createExplosion(event.getEntity().getLocation(), random_number, true, true, event.getEntity());
 			}
 		}
 	}
